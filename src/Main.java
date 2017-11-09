c8e0aa2b7321148c32af8a205611ad1f56158302
Rali <sfirlea.raluca@yahoo.com>
2017-11-09 17:49:20 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args)
+    {
+        int x = 1200, y = 5;
+        float a = 0;
+        a = x + y;
+        a = a / 2;
+        System.out.println("Media aritmetica a numerelor este " + a);
+    }
+}
